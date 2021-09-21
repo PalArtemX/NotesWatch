3cc0a438708dc5c6c4190cf06c062e8e1b97c90c
Artem Palyutin <76967266+PalArtemX@users.noreply.github.com>
2021-09-21 18:45:51 +0300

    Initial Commit

---
+++ NotesWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotesWatch WatchKit Extension
+//
+//  Created by Artem Palyutin on 21.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotesWatch WatchKit Extension/NotesWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesWatchApp.swift
+//  NotesWatch WatchKit Extension
+//
+//  Created by Artem Palyutin on 21.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct NotesWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
